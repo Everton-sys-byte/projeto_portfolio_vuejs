d451f05df479e72e374e884b2e0daaef2df7105f
Everton Soares Silva <everton_dev@hotmail.com>
2022-12-29 17:11:28 -0300

    Versão 1.0
    O projeto foi criado com sucesso.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="title">Teste</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+  /*diretivas do tailwindCSS*/
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+
+  .title{
+    /*classes*/
+    @apply text-xs italic text-center bg-slate-500
+  }
+</style>
